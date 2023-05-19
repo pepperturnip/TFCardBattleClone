8fef0b4835ba8cd924676776560d367768f19360
pepperturnip <pepperturnip@gmail.com>
2023-05-19 17:35:33 -0400

    Add TransitioningBasicCard

---
+++ Scripts/Core/TransitioningBasicCard.cs
@@ -0,0 +1,26 @@
+namespace TFCardBattle.Core
+{
+    public class TransitioningBasicCard : ICard
+    {
+        public string Name => "Basic";
+
+        public string Desc => "";
+
+        public CardPurchaseStats PurchaseStats => default;
+
+        public void Activate(BattleController battle)
+        {
+            // HACK: Add a different resource depending on how TF'd you are
+            // TODO: Don't transition _all_ basic cards at the same time!
+            // TODO: Update the card's name, too.
+            int tf = battle.State.PlayerTF;
+
+            if (tf < 33)
+                battle.State.Brain++;
+            else if (tf < 66)
+                battle.State.Heart++;
+            else
+                battle.State.Subs++;
+        }
+    }
+}
