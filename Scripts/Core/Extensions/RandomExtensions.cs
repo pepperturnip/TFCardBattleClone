e254cc9d1c0a37859b07b2384cb1be83db9a677c
pepperturnip <pepperturnip@gmail.com>
2023-05-19 16:02:41 -0400

    Add RandomExtensions.PickFromWeighted()

--- Scripts/Core/Extensions/RandomExtensions.cs
+++ Scripts/Core/Extensions/RandomExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TFCardBattle.Core
 {
@@ -8,6 +9,28 @@ namespace TFCardBattle.Core
         public static T PickFrom<T>(this Random rng, IReadOnlyList<T> items)
         {
             return items[rng.Next(items.Count)];
+        }
+
+        public static T PickFromWeighted<T>(
+            this Random rng,
+            params (T value, int weight)[] weights
+        )
+        {
+            int maxRoll = weights
+                .Select(w => w.weight)
+                .Sum();
+
+            int roll = rng.Next(maxRoll);
+
+            int sum = 0;
+            foreach (var w in weights)
+            {
+                sum += w.weight;
+                if (roll < sum)
+                    return w.value;
+            }
+
+            throw new Exception("Uhh...I didn't think this through, apparently");
         }
     }
 }
