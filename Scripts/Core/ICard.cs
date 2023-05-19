3000128a06423005e310986f17072d49987513d8
pepperturnip <pepperturnip@gmail.com>
2023-05-19 13:47:53 -0400

    Add basic stats to ICard

--- Scripts/Core/ICard.cs
+++ Scripts/Core/ICard.cs
@@ -4,5 +4,21 @@ namespace TFCardBattle.Core
     {
         string Name {get;}
         string Desc {get;}
+
+        int BrainCost {get;}
+        int HeartCost {get;}
+        int SubsCost {get;}
+
+        int MinTF {get;}
+        int MaxTF {get;}
+
+        /// <summary>
+        /// How likely this card is to be offered to the player for purchase,
+        /// if the player is within the TF range required for this card.
+        /// </summary>
+        /// <value></value>
+        int OfferWeight {get;}
+
+        void Activate(BattleState battleState);
     }
 }
