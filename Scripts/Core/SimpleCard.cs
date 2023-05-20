d557199c8bee9b9b0eb6dfda66cbe0327db56ca3
pepperturnip <pepperturnip@gmail.com>
2023-05-20 11:22:27 -0400

    SimpleCard now generates a description

--- Scripts/Core/SimpleCard.cs
+++ Scripts/Core/SimpleCard.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TFCardBattle.Core
 {
     /// <summary>
@@ -6,7 +8,7 @@ namespace TFCardBattle.Core
     public class SimpleCard : ICard
     {
         public string Name {get; set;}
-        public string Desc {get; set;}
+        public string Desc => GetDescription();
 
         public int BrainGain {get; set;}
         public int HeartGain {get; set;}
@@ -16,6 +18,9 @@ namespace TFCardBattle.Core
         public int CardDraw {get; set;}
 
         public CardPurchaseStats PurchaseStats {get; set;}
+
+        private string _description;
+        private int _descriptionHash;
 
         public void Activate(BattleController battle)
         {
@@ -28,6 +33,54 @@ namespace TFCardBattle.Core
             for (int i = 0; i < CardDraw; i++)
             {
                 battle.DrawCard();
+            }
+        }
+
+        private string GetDescription()
+        {
+            // Since this could get called every frame, don't re-generate the
+            // description unless something has changed.  This way we avoid
+            // creating extra work for the garbage collector.
+            int newHash = GetDescriptionHash();
+
+            if (newHash == _descriptionHash)
+                return _description;
+
+            // OK, the description doesn't match the resources.  Re-generate it.
+            var builder = new System.Text.StringBuilder();
+
+            LabelFor("Brain", BrainGain);
+            LabelFor("Heart", HeartGain);
+            LabelFor("Sub", SubGain);
+            LabelFor("Shield", ShieldGain);
+            LabelFor("TF", TFGain);
+            LabelFor("Draw", CardDraw);
+
+            _description = builder.ToString();
+            _descriptionHash = newHash;
+            return _description;
+
+            int GetDescriptionHash()
+            {
+                var hash = new HashCode();
+                hash.Add(BrainGain);
+                hash.Add(HeartGain);
+                hash.Add(SubGain);
+                hash.Add(ShieldGain);
+                hash.Add(TFGain);
+                hash.Add(CardDraw);
+
+                return hash.ToHashCode();
+            }
+
+            void LabelFor(string resource, int value)
+            {
+                if (value == 0)
+                    return;
+                else if (value > 0)
+                    builder.AppendLine($"{resource}: +{value}");
+                else
+                    builder.AppendLine($"{resource}: {value}");
             }
         }
     }
