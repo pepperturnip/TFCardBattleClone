35671be6a2e1b3d2eba4012cd3655452d584bced
pepperturnip <pepperturnip@gmail.com>
2023-05-28 19:40:09 -0400

    Use SceneTreeTimer instead of Task.Delay

--- Scripts/Godot/BattleAnimationPlayer.cs
+++ Scripts/Godot/BattleAnimationPlayer.cs
@@ -7,7 +7,13 @@ namespace TFCardBattle.Godot
 {
     public partial class BattleAnimationPlayer : Node, IBattleAnimationPlayer
     {
-        public Task DamageEnemy(int damageAmount) => Task.Delay(1000);
-        public Task DamagePlayer(int damageAmount) => Task.Delay(1000);
+        public Task DamageEnemy(int damageAmount) => Delay(1);
+        public Task DamagePlayer(int damageAmount) => Delay(1);
+
+        private async Task Delay(double seconds)
+        {
+            var timer = GetTree().CreateTimer(seconds);
+            await timer.ToSignal(timer, SceneTreeTimer.SignalName.Timeout);
+        }
     }
 }
