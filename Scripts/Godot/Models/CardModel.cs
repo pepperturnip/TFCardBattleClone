83c0f4ff20be5870ead736a5b26cfe2c124fce7d
pepperturnip <pepperturnip@gmail.com>
2023-06-03 11:48:47 -0400

    CardModel now exposes a Size property

--- Scripts/Godot/Models/CardModel.cs
+++ Scripts/Godot/Models/CardModel.cs
@@ -6,8 +6,11 @@ namespace TFCardBattle.Godot
 {
     public partial class CardModel : Node2D
     {
+        public Vector2 Size => _panel.Size;
+
         public ICard Card;
 
+        private Panel _panel => GetNode<Panel>("%Panel");
         private Label _nameLabel => GetNode<Label>("%NameLabel");
         private Label _descLabel => GetNode<Label>("%DescLabel");
         private CardCostDisplay _costDisplay => GetNode<CardCostDisplay>("%CardCostDisplay");
