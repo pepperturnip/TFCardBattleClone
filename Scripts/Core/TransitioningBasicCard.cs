31274962fca729fcf58cda761165dc21e4c87e57
pepperturnip <pepperturnip@gmail.com>
2023-06-13 12:33:15 -0400

    Add a big comment

--- Scripts/Core/TransitioningBasicCard.cs
+++ Scripts/Core/TransitioningBasicCard.cs
@@ -94,6 +94,38 @@ namespace TFCardBattle.Core
             return Task.CompletedTask;
         }
 
+        // Where did I get these constants from?  By experimenting with the
+        // original TF Card Battle game.  I went into sandbox mode, manipulated
+        // my TF, counted the number each basic card, and put it all into a
+        // spreadsheet, hoping that I could deduce a nice, simple formula to
+        // determine when cards transition.
+        //
+        // Unfortunately, the pattern is _almost_ nice, but there are some
+        // weird edge cases.  For example: in the original game, only one card
+        // can transition per turn, even if your TF jumps from 0 to 99 in one
+        // turn..._usually_.  Sometimes, more than one _will_ transition, and I
+        // don't have a goddamn clue why.  And sometimes, _no_ cards will
+        // transition, but then if you wait another turn at the same TF level,
+        // one of them _will_ suddenly transition, even though your TF level
+        // didn't change.
+        //
+        // Is there a bug in the original game?  Is this just a flaw in my
+        // experimental methodology?  Was I just too horny to notice an obvious
+        // pattern?  Probably all three!  But regardless, I decided to NOT try
+        // to match the original game's behavior exactly, and instead make my
+        // OWN rules, with blackjack and hookers!  These rules produce _similar_
+        // behavior to the original game, but with some tiny differences in some
+        // edge cases.  The most notable difference is that submits won't start
+        // appearing until TF level 45, instead of TF level 40.  A small price
+        // to pay for mathematical elegance.
+        //
+        // The rules I've implemented are as follows:
+        // * Below 10 TF, all basic cards are brain
+        // * Starting at 10 TF, one brain card becomes heart every 4 TF
+        // * Between 40 and 45 TF, all basic cards are heart
+        // * Starting at 45 TF, one heart card becomes sub every 5 TF
+        //      * No, that 5 is NOT a typo.  Hearts DO turn into submits slightly
+        //          slower than brains turn into hearts.
         private const int FlirtBeginTF = 10;
         private const int FlirtHoldTF = 40;
         private const int SubmitBeginTF = 45;
