39c1900b4f52a576527f343563cd6a1d3e7f9c96
pepperturnip <pepperturnip@gmail.com>
2023-06-16 16:46:45 -0400

    Add the Mental Mastery card

---
+++ Scripts/Core/CardClasses/MentalMastery.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+
+namespace TFCardBattle.Core.CardClasses
+{
+    public class MentalMastery : ICard
+    {
+        public string Name {get; set;}
+
+        public string Desc => "Remove a basic card from your deck.  Draw 1.";
+
+        public string TexturePath {get; set;}
+
+        public CardPurchaseStats PurchaseStats {get; set;}
+
+        public async Task Activate(BattleController battle)
+        {
+            await battle.ForgetBasicCard();
+            await battle.DrawCard();
+        }
+    }
+}
