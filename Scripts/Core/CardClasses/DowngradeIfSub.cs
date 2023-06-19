6c3b15e54aea590afc008f6c21b67bf4d3eb551a
pepperturnip <pepperturnip@gmail.com>
2023-06-19 19:20:01 -0400

    Add Purity of Lust

---
+++ Scripts/Core/CardClasses/DowngradeIfSub.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFCardBattle.Core.CardClasses
+{
+    public class DowngradeIfSub : ICard
+    {
+        public string Name {get; set;}
+        public string Desc => "TODO: How do I toggle this?";
+        public string TexturePath {get; set;}
+        public CardPurchaseStats PurchaseStats {get; set;}
+        public bool DestroyOnActivate {get; set;}
+
+        public Simple StrongVersion {get; set;}
+        public Simple WeakVersion {get; set;}
+
+        public Task Activate(BattleController battle)
+        {
+            return IsStrong(battle.State)
+                ? StrongVersion.Activate(battle)
+                : WeakVersion.Activate(battle);
+        }
+
+        public string GetTexturePath(BattleState state)
+        {
+            return IsStrong(state)
+                ? StrongVersion.GetTexturePath(state)
+                : WeakVersion.GetTexturePath(state);
+        }
+
+        private bool IsStrong(BattleState state)
+        {
+            return !state
+                .OwnedCards
+                .Any(c => c.PurchaseStats.SubCost > 0);
+        }
+    }
+}
