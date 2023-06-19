63224098e950948782ad4311b4655b1d0929826b
pepperturnip <pepperturnip@gmail.com>
2023-06-19 15:52:14 -0400

    Move the activate animation out of CardRowDisplay

--- Scripts/Godot/UIWidgets/CardRowDisplay.cs
+++ Scripts/Godot/UIWidgets/CardRowDisplay.cs
@@ -89,50 +89,6 @@ namespace TFCardBattle.Godot
             return cloneHolder;
         }
 
-        public void PlayActivateAnimation(int cardIndex)
-        {
-            // Make a copy of the card being removed, so we can animate it
-            // after removing it.
-            CardHolder cloneHolder = CloneCardForAnimation(cardIndex);
-
-            // Start animating the clone in the background.
-            const double stepDuration = 0.1;
-            var tween = GetTree().CreateTween();
-
-            tween.TweenProperty(
-                cloneHolder,
-                "position",
-                cloneHolder.Position + Vector2.Up * CardSize.Y,
-                stepDuration
-            );
-            tween.Parallel();
-            tween.TweenProperty(
-                cloneHolder.Scaler,
-                "scale",
-                Vector2.One * 1.1f,
-                stepDuration
-            );
-
-            tween.TweenProperty(
-                cloneHolder.Scaler,
-                "scale",
-                Vector2.One * 0.95f,
-                stepDuration
-            );
-            tween.TweenProperty(
-                cloneHolder.Scaler,
-                "scale",
-                Vector2.One,
-                stepDuration
-            );
-            tween.TweenProperty(
-                cloneHolder,
-                "modulate",
-                Colors.Transparent,
-                stepDuration);
-            tween.TweenCallback(new Callable(cloneHolder, "queue_free"));
-        }
-
         public void Refresh(ICard[] cards)
         {
             // HACK: Reuse old models if nothing changed, to prevent the card
