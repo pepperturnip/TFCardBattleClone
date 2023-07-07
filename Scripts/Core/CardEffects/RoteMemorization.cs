f39f0c5d237e3aa63d897c0b4ed0041318205519
pepperturnip <pepperturnip@gmail.com>
2023-07-06 21:39:56 -0400

    Fix RoteMemorization not counting cards in your discard or in-play pile

--- Scripts/Core/CardEffects/RoteMemorization.cs
+++ Scripts/Core/CardEffects/RoteMemorization.cs
@@ -13,12 +13,19 @@ namespace TFCardBattle.Core.CardEffects
             // in the in-play list before calling Activate().
             Card copy = battle.State.CardsPlayedThisTurn.Last();
 
+            // Add brain equal to the number of copies the player owns.
+            //
+            // This is technically different from what the original game does.
+            // The original game increments a counter every time you play this
+            // card, and that counter is what gets added to your brain.
+            //
+            // I'd rather not add a separate field to BattleState just for this
+            // one card, though, so let's just count the copies owned instead.
+            // And, honestly, this way is just more intuitive.
+            battle.State.Brain += battle.State.OwnedCards.Count(c => c == copy);
+
             // Add a copy of this card to the discard pile
             battle.State.Discard.Add(copy);
-
-            // Add brain equal to the number of copies in the deck
-            battle.State.Brain += battle.State.Deck.Count(c => c == copy);
-
             return Task.CompletedTask;
         }
 
