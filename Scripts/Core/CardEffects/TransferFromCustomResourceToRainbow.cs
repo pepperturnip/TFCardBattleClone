a431247c88ec1db8b9550bf02441e0b96f17c8d9
pepperturnip <pepperturnip@gmail.com>
2023-07-21 18:48:26 -0400

    Fill in all the hucow card effects

---
+++ Scripts/Core/CardEffects/TransferFromCustomResourceToRainbow.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+
+namespace TFCardBattle.Core.CardEffects
+{
+    public class TransferFromCustomResourceToRainbow : ICardEffect
+    {
+        public CustomResourceId Resource;
+
+        public Task Activate(BattleController battle)
+        {
+            int from = (int)Math.Floor(battle.State.CustomResources[Resource]);
+            battle.State.Brain += from;
+            battle.State.Heart += from;
+            battle.State.Sub += from;
+
+            return Task.CompletedTask;
+        }
+
+        public string GetDescription(BattleState state)
+            => $"{state.CardRegistry.CustomResources[Resource].Name} => Rainbow";
+
+        public string GetOverriddenImage(BattleState state) => null;
+    }
+}
