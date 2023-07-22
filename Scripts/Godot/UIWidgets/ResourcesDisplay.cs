3cc62faff6c13ed3bdb2863f4e2996e795bfa5f2
pepperturnip <pepperturnip@gmail.com>
2023-07-21 20:20:43 -0400

    ResourcesDisplay now picks up custom resources as well

--- Scripts/Godot/UIWidgets/ResourcesDisplay.cs
+++ Scripts/Godot/UIWidgets/ResourcesDisplay.cs
@@ -8,6 +8,7 @@ namespace TFCardBattle.Godot
     public partial class ResourcesDisplay : Control
     {
         private readonly ResourceType[] _resources = Enum.GetValues<ResourceType>();
+        private readonly HashSet<CustomResourceId> _customResources = new HashSet<CustomResourceId>();
 
         private Control _template;
 
@@ -29,6 +30,13 @@ namespace TFCardBattle.Godot
                 var label = GetValueLabel(resource);
                 label.AccumulateToValue(state.GetResource(resource));
             }
+
+            foreach (var id in state.CustomResources.Keys)
+            {
+                _customResources.Add(id);
+                var label = GetValueLabel(id);
+                label.AccumulateToValue(state.CustomResources[id]);
+            }
         }
 
         /// <summary>
@@ -42,6 +50,9 @@ namespace TFCardBattle.Godot
                 var label = GetValueLabel(resource);
                 label.RefreshValue(0);
             }
+
+            // Note: we're intentionally NOT discarding the custom resources,
+            // because they don't reset at the end of every turn.
         }
 
         public override void _Process(double delta)
@@ -50,6 +61,13 @@ namespace TFCardBattle.Godot
             {
                 var resourceDisplay = GetResourceDisplay(resource);
                 var valueLabel = GetValueLabel(resource);
+                resourceDisplay.Visible = valueLabel.DisplayedValue != 0 || valueLabel.AccumulatedDelta != 0;
+            }
+
+            foreach (var id in _customResources)
+            {
+                var resourceDisplay = GetResourceDisplay(id);
+                var valueLabel = GetValueLabel(id);
                 resourceDisplay.Visible = valueLabel.DisplayedValue != 0 || valueLabel.AccumulatedDelta != 0;
             }
         }
@@ -70,6 +88,24 @@ namespace TFCardBattle.Godot
         }
 
         private AccumulatingLabel GetValueLabel(ResourceType resource)
+            => GetResourceDisplay(resource).GetNode<AccumulatingLabel>("ValueLabel");
+
+        private Control GetResourceDisplay(CustomResourceId resource)
+        {
+            var control = GetNodeOrNull<Control>(resource.ToString());
+            if (control != null)
+                return control;
+
+            // It doesn't already exist, so create it from the template
+            var resourceDisplay = (Control)_template.Duplicate();
+            resourceDisplay.Name = resource.ToString();
+            resourceDisplay.GetNode<Label>("Label").Text = resource.ToString();
+            AddChild(resourceDisplay);
+
+            return resourceDisplay;
+        }
+
+        private AccumulatingLabel GetValueLabel(CustomResourceId resource)
             => GetResourceDisplay(resource).GetNode<AccumulatingLabel>("ValueLabel");
     }
 }
