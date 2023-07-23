74e1f2f673d7baec9239ae1e0bee0e599413d8b8
pepperturnip <pepperturnip@gmail.com>
2023-07-23 14:56:57 -0400

    Consumables now have icons associated with them in the json

--- Scripts/Core/Consumable.cs
+++ Scripts/Core/Consumable.cs
@@ -5,6 +5,7 @@ namespace TFCardBattle.Core
     public class Consumable
     {
         public string Name;
+        public string IconPath;
 
         [JsonConverter(typeof(Parsing.CardEffectJsonConverter))]
         public ICardEffect Effect;
