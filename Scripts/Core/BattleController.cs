7ea3a1b1e6bd93fca0f3c31b6d9153c200f57611
pepperturnip <pepperturnip@gmail.com>
2023-08-05 15:45:42 -0400

    Move ForgetBasicCard() out of BattleController and into the ForgetBasic effect.

--- Scripts/Core/BattleController.cs
+++ Scripts/Core/BattleController.cs
@@ -285,46 +285,6 @@ namespace TFCardBattle.Core
             await TriggerEffects(e => e.OnResourcesDiscarded(this));
         }
 
-        public async Task ForgetBasicCard()
-        {
-            if (await TryForgetFrom(State.Deck))
-                return;
-
-            if (await TryForgetFrom(State.Discard))
-                return;
-
-            if (await TryForgetFrom(State.InPlay))
-                return;
-
-            if (await TryForgetFrom(State.Hand))
-                return;
-
-            // TODO: Show some message saying no card could be forgotten
-
-            bool IsBasic(Card c)
-            {
-                // TODO: Allow Mysterious Pills to be deleted as well
-                return c.Effect is CardEffects.TransitioningBasicCard;
-            }
-
-            async Task<bool> TryForgetFrom(List<Card> cards)
-            {
-                for (int i = 0; i < cards.Count; i++)
-                {
-                    var card = cards[i];
-
-                    if (IsBasic(card))
-                    {
-                        cards.RemoveAt(i);
-                        await AnimationPlayer.ForgetCard(card, State);
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-        }
-
         public async Task TakeExtraTurn()
         {
             // For the Triumph of the Mind card.
--- Scripts/Core/CardEffects/ForgetBasic.cs
+++ Scripts/Core/CardEffects/ForgetBasic.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace TFCardBattle.Core.CardEffects
@@ -6,7 +8,44 @@ namespace TFCardBattle.Core.CardEffects
     {
         public async Task Activate(BattleController battle)
         {
-            await battle.ForgetBasicCard();
+            var state = battle.State;
+
+            if (await TryForgetFrom(state.Deck))
+                return;
+
+            if (await TryForgetFrom(state.Discard))
+                return;
+
+            if (await TryForgetFrom(state.InPlay))
+                return;
+
+            if (await TryForgetFrom(state.Hand))
+                return;
+
+            // TODO: Show some message saying no card could be forgotten
+
+            bool IsBasic(Card c)
+            {
+                // TODO: Allow Mysterious Pills to be deleted as well
+                return c.Effect is CardEffects.TransitioningBasicCard;
+            }
+
+            async Task<bool> TryForgetFrom(List<Card> cards)
+            {
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    var card = cards[i];
+
+                    if (IsBasic(card))
+                    {
+                        cards.RemoveAt(i);
+                        await battle.AnimationPlayer.ForgetCard(card, state);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
         }
 
         public string GetDescription(BattleState state)
