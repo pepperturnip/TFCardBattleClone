f0caf688c251e24e07383f29d86d02842300cccb
pepperturnip <pepperturnip@gmail.com>
2023-08-26 15:13:57 -0400

    Add RelicSelectCardModel

---
+++ Scripts/Godot/Models/RelicSelectCardModel.cs
@@ -0,0 +1,24 @@
+using System;
+using TFCardBattle.Core;
+using Godot;
+
+namespace TFCardBattle.Godot
+{
+    public partial class RelicSelectCardModel : Control
+    {
+        public void SetRelic(RelicId relicId)
+        {
+            var relic = ContentRegistry.Relics[relicId];
+
+            GetNode<Label>("%NameLabel").Text = relic.Name;
+            GetNode<Label>("%DescLabel").Text = relic.Description;
+            GetNode<Label>("%CostLabel").Text = relic.TFCost.ToString();
+
+            if (ResourceLoader.Exists(relic.IconPath))
+            {
+                var texture = ResourceLoader.Load<Texture2D>(relic.IconPath);
+                GetNode<TextureRect>("%Art").Texture = texture;
+            }
+        }
+    }
+}
